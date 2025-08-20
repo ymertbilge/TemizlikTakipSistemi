ac6258a9ab62eb7d75c89373106d0a77ed1b6718
Yağız Mert Bilge <yagizmertbilge@gmail.com>
2025-08-20 23:33:51 +0300

    Update

--- frontend/src/services/firebaseService.js
+++ frontend/src/services/firebaseService.js
@@ -1,23 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, get, set, push, remove, update, query, orderByChild, equalTo } from 'firebase/database';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-
-// Firebase yapılandırması - .env dosyasından alınacak
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-};
-
-// Firebase'i başlat
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth(app);
+import { ref, get, set, push, remove, update, query, orderByChild, equalTo } from 'firebase/database';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import { database, auth } from '../config/firebase';
 
 // Firebase Realtime Database referansları - Firebase v9 syntax
 const getReportsRef = () => ref(database, 'temizlikTakip/reports');
